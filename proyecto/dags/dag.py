fafae848920b5b6b1f6f51f61c82c4ee1290366b
Benjamin Daza Jimenez <benja.daza.jimenez@gmail.com>
2025-07-04 23:31:56 -0400

    Terminado

--- proyecto/dags/dag.py
+++ proyecto/dags/dag.py
@@ -1,11 +1,10 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 # Parámetros
 INPUT_PATH = '/proyecto/data/*.json'
-OUTPUT_PATH = '/proyecto/output'
+OUTPUT_PATH = '/proyecto/output/'
 PYTHON_SCRIPT_PATH = '/proyecto/src/etl_batch.py'
 
 default_args = {
@@ -18,15 +17,15 @@ with DAG(
     dag_id='etl_jsonl_to_parquet',
     default_args=default_args,
     description='Pipeline ETL con Apache Beam (local) para convertir JSONL a Parquet',
-    schedule_interval="@daily",
-    start_date=days_ago(1),
+    schedule='@daily',
+    start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=['etl', 'beam', 'parquet'],
 ) as dag:
 
     run_beam_etl = BashOperator(
         task_id='run_beam_etl',
-        bash_command=f'python3 {PYTHON_SCRIPT_PATH} --input_path {INPUT_PATH} --output_path {OUTPUT_PATH}',
+        bash_command=f'python3 {PYTHON_SCRIPT_PATH} --input_path {INPUT_PATH} --output_path {OUTPUT_PATH} --runner DirectRunner',
     )
 
     run_beam_etl
